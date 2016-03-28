11f905d959eb30a27f34834f1c7343fbb3738858
azemoh <joshazemoh@gmail.com>
2016-03-28 16:05:43 +0100

    Done.

--- models/textanalyzer.rb
+++ models/textanalyzer.rb
@@ -1,1 +1,30 @@
-#your text analyzer model code will go here.
+class TextAnalyzer
+  attr_accessor :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def count_of_words
+    @text.split(" ").count
+  end
+
+  def count_of_vowels
+    @text.downcase.scan(/[aeiou]/).count
+  end
+
+  def count_of_consonants
+    @text.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+    letters = @text.downcase.gsub(/[^a-z]/, '').split('')
+    keys = letters.uniq
+    letters_count = {}
+    keys.each { |k| letters_count[k] = letters.count(k) }
+    most_used = letters_count.sort_by { |letter, count| count }.last
+
+    {:most_used_letter => most_used[0].upcase, :letter_count => most_used[1]}
+  end
+
+end
