1707fb8f4c296e0fe531b10f5d234c3ecf4d28bd
glee38 <glee3388@gmail.com>
2016-03-29 17:37:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,18 @@ get '/' do
 end
 
 post '/' do
-  text_from_user = params[:user_text]
+  # text_from_user = params[:user_text]
+  # @analyzed_text = TextAnalyzer.new(text_from_user)
+
+  # or
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
+
+  @num_of_words = @analyzed_text.count_of_words
+  @vowels = @analyzed_text.count_of_vowels
+  @consonants = @analyzed_text.count_of_consonants
+  @most_com_letter = @analyzed_text.most_used_letter
+
+  raise @analyzed_text.text.inspect
   erb :results
 end
 
