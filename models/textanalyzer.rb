6ef67c8d10544447e98544d84028f612cd3f2481
jbok4 <jbok4@aol.com>
2016-05-11 19:22:00 -0400

    complete

--- models/textanalyzer.rb
+++ models/textanalyzer.rb
@@ -18,19 +18,21 @@ class TextAnalyzer
   end
  
 def most_used_letter
-    all_letters_in_string = @text.downcase.gsub(/[^a-z]/, '').split('') #gets rid of spaces and turns it into an array
-    letters_to_compare = all_letters_in_string.uniq
-    most_used_letter = ""
-    letter_count = 0
- 
-    letters_to_compare.map do |letter|
-      letter_repetitions = all_letters_in_string.count(letter)
-      if letter_repetitions > letter_count
-        letter_count = letter_repetitions
-        most_used_letter = letter
+    s1 = @text.downcase.gsub(/[^a-z]/, '') #gets rid of spaces
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+    arr1.map do |c| 
+     arr2[c] =  arr.count(c)
+    end
+    biggest = { arr2.keys.first  => arr2.values.first }
+    arr2.each do |key, value|
+      if value > biggest.values.first
+        biggest = {}
+        biggest[key] = value
       end
     end
-    biggest = [most_used_letter, letter_count.to_i]
+    biggest
   end
  
 end
