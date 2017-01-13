462c0a5845b9ad964013314cee896115c62d52ab
Michael Burak <michaelrburak@gmail.com>
2017-01-13 18:06:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ get '/' do
   erb :index
 end
 
-post '/' do
-  text_from_user = params[:user_text]
+
+  post '/' do
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
   erb :results
 end
-
 
 end
