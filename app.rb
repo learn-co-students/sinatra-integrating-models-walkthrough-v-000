e65a8a625f59fd2b2bf376b71e521aeffff26d20
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-09-18 10:55:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
 
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @letter = @analyzed_text.most_used_letter.keys[0].upcase
+    @amount_used = @analyzed_text.most_used_letter.values[0]
+    "#{@letter}"
+    "#{@amount_used}"
     erb :results
   end
 end
