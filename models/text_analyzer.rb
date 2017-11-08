d5915b87ffd36d9b0467fd1b0b40ddaa1cb570b9
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-08 18:15:38 +0000

    Done.

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,43 @@
-# Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+  end
+
+  def count_of_words
+    self.text.split(" ").count
+  end
+
+  def count_of_vowels
+    self.text.scan(/[aeiou]/).count
+  end
+
+  def count_of_consonants
+    self.text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+    self.text.split(" ").join("").split("")
+
+    letter_comparison={}
+    letters_to_check = self.text.split(" ").join("").split("").uniq
+    letters_to_check.each do |letter|
+      text.split(" ").join("").split("").select {|x| x == letter }.length
+      letter_comparison[letter]=text.split(" ").join("").split("").select {|x| x == letter }.length
+    end
+    max_letter="a"
+    max_letter_number=0
+
+    letter_comparison.each {|key,value|
+      if value>max_letter_number
+        max_letter=key
+        max_letter_number=value
+      end
+    }
+    most_used_letter =[max_letter,max_letter_number]
+    
+  end
+
+
+end
