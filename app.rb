d21808deccae893a1c8caf5f8923a057448871a7
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-14 21:59:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
-  get '/' do
+  get '/' do #gets data from screen
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
-
+  post '/' do #posts to screen
+    # text_from_user = params[:user_text]
+    # @analyzed_text = TextAnalyzer.new(text_from_user)
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
     erb :results
   end
 end
