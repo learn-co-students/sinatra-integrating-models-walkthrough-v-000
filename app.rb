b0e5fa28545e0dc589eb196b7dbdc103813d1f7e
github username <github email address>
2018-01-24 08:42:37 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,12 +2,20 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+#renders the index.erb page
   get '/' do
     erb :index
   end
 
+#receives the form data through params and renders the results page
   post '/' do
-    text_from_user = params[:user_text]
+#the instance of TextAnalyzer saved to an instance variable @analyzed_text
+#we can call it and its methods from the results.erb view that is being
+#rendered, using erb tags   
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    # above line of code simplifies the bottom two lines of code
+    # text_from_user = params[:user_text]
+    # @analyzed_text = TextAnalyzer.new(text_from_user)
 
     erb :results
   end
