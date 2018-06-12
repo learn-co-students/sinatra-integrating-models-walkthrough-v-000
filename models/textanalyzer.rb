116910c9fdc33c679461435d00caa7c60ca5a9c1
osxgirl <osxgirl@mac.com>
2018-06-12 10:32:11 -0400

    //❤️

---
+++ models/textanalyzer.rb
@@ -0,0 +1,46 @@
+class TextAnalyzer
+    attr_reader :text
+    
+    def initialize(text)
+        @text = text.downcase
+    end
+    
+    def count_of_words
+        words = text.split(" ")
+        words.count
+    end
+    
+    def count_of_vowels
+        text.scan(/[aeoui]/).count
+    end
+    
+    def count_of_consonants
+        text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+    end
+    
+    def most_used_letter
+        s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
+        arr = s1.split('')
+        arr1 = arr.uniq
+        arr2 = {}
+        
+        arr1.map do |c|
+            arr2[c] =  arr.count(c)
+        end
+        
+        biggest = { arr2.keys.first => arr2.values.first }
+        
+        arr2.each do |key, value|
+            if value > biggest.values.first
+                biggest = {}
+                biggest[key] = value
+            end
+        end
+        
+        biggest
+    end
+
+end
+
+
+#The model above has an initializer which takes in a string text, converts it to lowercase, and saves it to an instance variable @text. This instance variable is then used in the four instance methods, which provide information on the block of text in question
