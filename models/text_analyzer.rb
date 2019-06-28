d0e6e511156fbdd69b9ff7e0c945c10aab1de3e5
Steven Crouse <sdcrouse91@gmail.com>
2019-06-28 05:25:24 +0000

    Done.

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,27 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end 
+  
+  def count_of_words
+    words = text.strip.split(" ") # I added #strip for an edge case.
+    words.count 
+  end
+  
+  def count_of_vowels
+    text.scan(/[aeiou]/).count 
+    # There are probably other ways, but this is quickest/easiest (I think).
+  end
+  
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+    # I could have tried something like text.scan(/[^aeiou]/), but that would have ALSO picked up numbers, \n, white space, characters, etc.
+  end
+  
+  def most_used_letter
+    
+  end
+end
