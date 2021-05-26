2a2aaf9453d9dacb3aafbcd4870a879799b4ecb2
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-25 23:11:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
+
 class App < Sinatra::Base
   get '/' do
     erb :index
@@ -9,7 +10,6 @@ class App < Sinatra::Base
   post '/' do
     @analyzed_text = 
     TextAnalyzer.new(params[:user_text])
-
     erb :results
   end
 end
